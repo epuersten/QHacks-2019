0570c513952dd027a2f5841ad9ecd09d87c76cb3
Aditya Dutta <aditya.dutta10@gmail.com>
2019-02-02 21:10:33 -0500

    updated registration v2

--- app/routes_authentication.py
+++ app/routes_authentication.py
@@ -4,7 +4,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, abort
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm #ResetPasswordRequestForm, ResetPasswordForm
+# ResetPasswordRequestForm, ResetPasswordForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 # from app.email import send_email_password_reset
 from app.models import NetUser
 
@@ -12,6 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import migrate
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
+import os
 from datetime import datetime
 
 
@@ -19,24 +22,23 @@ from datetime import datetime
 @app.route('/index',  methods=['GET', 'POST'])
 def index():
     html = {}
-    html['title']          = "Welcome back - Login"
-    html['description']    = "Welcome back. Nice to see you."
-    html['content']        = "LoggedIn"
+    html['title'] = "Welcome back - Login"
+    html['description'] = "Welcome back. Nice to see you."
+    html['content'] = "LoggedIn"
     return render_template('user.html', html=html)
-
 
 
 ##############################
 # PUBLIC
 ##############################
-#registration page
+# registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 
     html = {}
-    html['title']          = "Register"
-    html['description']    = "Join us today"
-    html['content']        = ""
+    html['title'] = "Register"
+    html['description'] = "Join us today"
+    html['content'] = ""
 
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -44,25 +46,40 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        user = NetUser(email=form.email.data, first_name=form.first_name.data, last_name=form.last_name.data, program_name=form.program.data, school_name=form.school.data)
+        user = NetUser(email=form.email.data, first_name=form.first_name.data,
+                       last_name=form.last_name.data, program_name=form.program.data, school_name=form.school.data)
         user.set_password(form.password1.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
 
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
     return render_template('register_new.html',
                            html=html,
                            form=form)
 
-
-#route for login
+# route for login
 @app.route('/', methods=['GET', 'POST'])
 def login():
 
     html = {}
-    html['title']          = "Welcome back - Login"
-    html['description']    = "Welcome back. Nice to see you."
-    html['content']        = ""
+    html['title'] = "Welcome back - Login"
+    html['description'] = "Welcome back. Nice to see you."
+    html['content'] = ""
 
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -87,22 +104,22 @@ def login():
                            html=html,
                            form=form)
 
-#logout route
+# logout route
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-#edit user profile
+# edit user profile
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
 
     html = {}
-    html['title']          = "My Profile"
-    html['description']    = ""
-    html['content']        = ""
+    html['title'] = "My Profile"
+    html['description'] = ""
+    html['content'] = ""
 
     # Get user profile and record
     user_id = NetUser.user_id
@@ -129,25 +146,27 @@ def profile():
                            form=form,
                            user=user)
 
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect(url_for('uploaded_file',
-#                                     filename=filename))
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
